0c35e5ef9848b948c2757cbd68db984283220a93
IharSha <qrutas@gmail.com>
2020-12-27 19:34:44 +0200

    use .env instead local_settings;

--- buildHeroProject/settings.py
+++ buildHeroProject/settings.py
@@ -25,11 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-try:
-    from local_settings import SECRET_KEY, DEBUG
-except ImportError:
-    SECRET_KEY = get_random_secret_key()
-    DEBUG = True
+SECRET_KEY = get_random_secret_key()
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
