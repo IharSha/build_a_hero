91065484e74b4e6e1e3d225e16a9b5537cc0d64a
IharSha <qrutas@gmail.com>
2020-12-27 17:38:47 +0200

    update project to be able to deploy to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn buildHeroProject.wsgi --log-file -
--- buildHeroProject/settings.py
+++ buildHeroProject/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MESSAGE_TAGS = {
     messages.DEBUG: 'alert-info',
