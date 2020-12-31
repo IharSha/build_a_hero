d6ef12484e7d960d752546fe9348ca80faf891ba
IharSha <qrutas@gmail.com>
2020-12-31 22:40:01 +0200

    Select active character;

--- wwwhero/admin.py
+++ wwwhero/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 
-from .models import Character, CharacterAttributes, UserVisit
+from .models import Character, CharacterAttributes, CharacterSelection, UserVisit
 
 
 class CharacterAdmin(admin.ModelAdmin):
     list_display = ('name', 'created_at', 'updated_at')
 
 
+class CharacterSelectionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'character')
+
+
 admin.site.register(Character, CharacterAdmin)
 admin.site.register(CharacterAttributes)
+admin.site.register(CharacterSelection, CharacterSelectionAdmin)
 admin.site.register(UserVisit)
