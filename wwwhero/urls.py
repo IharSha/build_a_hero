2e14a8463e8d153ba930f153d85ed9b5645caa88
IharSha <qrutas@gmail.com>
2021-01-09 15:27:42 +0200

    Location model and supportive things added; Character location model added; Ability to select character location added; Map page added with locations; Story page placeholder added; Smarter redirections; Remember character_id trhough session now;

--- wwwhero/urls.py
+++ wwwhero/urls.py
@@ -5,10 +5,13 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('characters/<int:character_id>/', views.character_select, name='character_select'),
-    path('character_detail/', views.character_detail, name='character_detail'),
-    path('character_create/', views.character_create_view, name='character_create'),
+    path('character/detail/', views.character_detail_view, name='character_detail'),
+    path('character/create/', views.character_create_view, name='character_create'),
     path('character/levelup/', views.character_level_up, name='character_level_up'),
-    path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('map/', views.map_view, name='map'),
+    path('locations/<int:location_id>/', views.location_select, name='location_select'),
+    path('story/', views.story_view, name='story'),
+    path('accounts/signup/', views.signup_view, name='signup'),
+    path('accounts/login/', views.login_view, name='login'),
+    path('accounts/logout/', views.logout_view, name='logout'),
 ]
